57a6795166f36149e83ad55f408b6a7fabc50d6a
Jose <84431245+jocmamorales@users.noreply.github.com>
2024-10-26 00:54:17 -0300

    Se crea la pantalla de informe de obstaculos. Los spinners estan harcodeados.

--- app/src/main/java/com/example/senderosseguros/ui/reporte/ReporteFragment.java
+++ app/src/main/java/com/example/senderosseguros/ui/reporte/ReporteFragment.java
@@ -4,12 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.senderosseguros.R;
 import com.example.senderosseguros.databinding.FragmentReporteBinding;
 
 public class ReporteFragment extends Fragment {
@@ -24,9 +25,30 @@ public class ReporteFragment extends Fragment {
         binding = FragmentReporteBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textReporte;
-        reporteViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        // Inicializar los Spinners
+        setupSpinners();
+
         return root;
+    }
+
+    private void setupSpinners() {
+        // Spinner para "Barrio"
+        ArrayAdapter<CharSequence> barrioAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.barrio_options, android.R.layout.simple_spinner_item);
+        barrioAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.spinnerBarrio.setAdapter(barrioAdapter);
+
+        // Spinner para "Tipo de Obstáculo"
+        ArrayAdapter<CharSequence> tipoAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.tipo_options, android.R.layout.simple_spinner_item);
+        tipoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.spinnerObstaculo.setAdapter(tipoAdapter);
+
+        //  Spinner para "Periodo"
+        ArrayAdapter<CharSequence> periodoAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.periodo_options, android.R.layout.simple_spinner_item);
+        periodoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.spinnerTiempo.setAdapter(periodoAdapter);
     }
 
     @Override
