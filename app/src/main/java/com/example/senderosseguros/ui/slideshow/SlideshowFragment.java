5d119cc2e734d22e706c9496590dcd7956033b23
tfdace <tf.acedo@gmail.com>
2024-10-26 23:24:16 -0300

    Se agrega ubicacion por default al cargar la aplicacion

--- app/src/main/java/com/example/senderosseguros/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/senderosseguros/ui/slideshow/SlideshowFragment.java
@@ -1,41 +1,67 @@
 package com.example.senderosseguros.ui.slideshow;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
 
 import com.example.senderosseguros.R;
 import com.example.senderosseguros.databinding.FragmentSlideshowBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class SlideshowFragment extends Fragment implements OnMapReadyCallback {
 
     private FragmentSlideshowBinding binding;
     private GoogleMap mMap;
+    private FusedLocationProviderClient fusedLocationClient;
+    private static final String TAG = "Mis mapas";
 
+    // Register the permissions launcher
+    private final ActivityResultLauncher<String[]> requestPermissionsLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), result -> {
+                Boolean fineLocationGranted = result.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false);
+                Boolean coarseLocationGranted = result.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false);
+
+                if (fineLocationGranted != null && fineLocationGranted) {
+                    enableLocation();
+                } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                    enableLocation();
+                } else {
+                    Log.i(TAG, "Permisos de ubicacion denegados.");
+                }
+            });
+
+    @Nullable
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        SlideshowViewModel slideshowViewModel = new ViewModelProvider(this).get(SlideshowViewModel.class);
-
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentSlideshowBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapView);
-        final TextView tvLat = binding.tvLat;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), tvLat::setText);
-        final TextView tvLong = binding.tvLong;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), tvLong::setText);
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
+
         return root;
     }
 
@@ -49,12 +75,45 @@ public class SlideshowFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Configurar los controles del mapa y otras opciones
         UiSettings uiSettings = mMap.getUiSettings();
         uiSettings.setZoomControlsEnabled(true);
         uiSettings.setMyLocationButtonEnabled(true);
         uiSettings.setCompassEnabled(true);
         uiSettings.setTiltGesturesEnabled(true);
         uiSettings.setRotateGesturesEnabled(true);
+
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            enableLocation();
+        } else {
+            requestPermissionsLauncher.launch(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            });
+        }
+    }
+
+    private void enableLocation() {
+        try {
+            mMap.setMyLocationEnabled(true);
+            getCurrentLocation();
+        } catch (SecurityException e) {
+            Log.e(TAG, "Verifique los permisos de ubicacion: ", e);
+        }
+    }
+
+    private void getCurrentLocation() {
+        try {
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(requireActivity(), location -> {
+                        if (location != null) {
+                            LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                            mMap.addMarker(new MarkerOptions().position(currentLocation).title("Mi ubicacion"));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15f));
+                        }
+                    });
+        } catch (SecurityException e) {
+            Log.e(TAG, "Verifique los permisos de ubicacion: ", e);
+        }
     }
 }
